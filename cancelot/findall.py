18553e8e816735fe6d75bf45bb269c3c21fa24f7
Noel Maersk <veox+gits@veox.pw>
2017-05-23 18:01:32 +0300

    findall: factor out check_receipt_for_topics() + use dictionary for topics.

--- cancelot/findall.py
+++ cancelot/findall.py
@@ -8,32 +8,36 @@ from web3 import Web3, IPCProvider
 registrar = '0x6090a6e47849629b7245dfa1ca21d94cd15878ef'
 startblock = 3648565
 
-# topic fingerprints
-fp_auctionstarted = '0x87e97e825a1d1fa0c54e1d36c7506c1dea8b1efd451fe68b000cf96f7cf40003'
-fp_newbid = '0xb556ff269c1b6714f432c36431e2041d28436a73b6c3f19c021827bbdc6bfc29'
-fp_bidrevealed = '0x7b6c4b278d165a6b33958f8ea5dfb00c8c9d4d0acf1985bef5d10786898bc3e7'
+# topics: fingerprints -> event name
+topics = {
+    #'0x87e97e825a1d1fa0c54e1d36c7506c1dea8b1efd451fe68b000cf96f7cf40003': 'AuctionStarted',
+    '0xb556ff269c1b6714f432c36431e2041d28436a73b6c3f19c021827bbdc6bfc29': 'NewBid',
+    '0x7b6c4b278d165a6b33958f8ea5dfb00c8c9d4d0acf1985bef5d10786898bc3e7': 'BidRevealed'
+}
 
 
 web3 = Web3(IPCProvider())
 
+def check_receipt_for_topics(receipt, topics):
+    logs = receipt['logs']
+    # iterate through events, looking for bids placed/revealed fingerprint
+    for entry in logs:
+        fp = entry['topics'][0]
+        topic = topics[fp] if topics.get(fp) else False
+        if topic:
+            print('tx', tx['hash'],
+                  'in block', tx['blockHash'], '(' + str(tx['blockNumber']) + ')',
+                  'has event', topic)
+    return
+
 def check_tx(tx):
     receipt = web3.eth.getTransactionReceipt(tx['hash'])
-    logs = receipt['logs']
 
     # short-circuit if no event logs (probably OOGed)
-    if len(logs) == 0: return
+    if len(receipt['logs']) == 0: return
 
-    # iterate through events, looking for bids placed/revealed fingerprint
-    for l in logs:
-        fp = l['topics'][0]
-        if fp == fp_newbid:
-            print('tx', tx['hash'],
-                  'in', tx['blockHash'], '(' + str(tx['blockNumber']) + ')',
-                  'placed a bid')
-        if fp == fp_bidrevealed:
-            print('tx', tx['hash'],
-                  'in', tx['blockHash'], '(' + str(tx['blockNumber']) + ')',
-                  'revealed a bid')
+    check_receipt_for_topics(receipt, topics)
+
     return
 
 
