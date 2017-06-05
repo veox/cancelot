9b66ba9452858af66ba5ce2215c350e9398c3213
Noel Maersk <veox+gits@veox.pw>
2017-06-05 18:23:41 +0300

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os.path
+from distutils.core import setup
+
+exec(open('./cancelot/version.py').read())
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='cancelot',
+      version=__version__,
+      description='ENS bid monitoring and cancellation utilities',
+      long_description=read('README.rst'),
+      author='Noel Maersk',
+      author_email='veox+packages+spamremove@veox.pw',
+      url=__url__,
+      packages=['cancelot'],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+      ],
+)
