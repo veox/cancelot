6fce38abf4665bbcf0732f3b8fb09b8b58e24df2
Noel Maersk <veox+gits@veox.pw>
2017-06-08 13:34:43 +0300

    run-oneshot.sh: minor, `*pickle` -> `*.pickle`.

--- run-oneshot.sh
+++ run-oneshot.sh
@@ -5,7 +5,7 @@ mkdir -p logs pickles
 latest="pickles/`ls -1t pickles/ | head -1`"
 python -u oneshot.py $latest 2>&1 | tee -i -a logs/`date +%s`.log
 
-latest="`ls -1t *pickle | head -1`"
+latest="`ls -1t *.pickle | head -1`"
 mv $latest pickles/
 rm -f *.pickle
 
