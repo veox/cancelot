65b239decac4bd088c5fcf875d3a015fd09db853
Noel Maersk <veox+gits@veox.pw>
2017-06-09 15:46:53 +0300

    strategy: remove from package altogether.

--- cancelot/__init__.py
+++ cancelot/__init__.py
@@ -2,6 +2,5 @@ from .bidinfo import BidInfo
 from .bidstore import BidStore
 
 from . import utils
-from . import strategy
 
-__all__ = [BidInfo, BidStore, utils, strategy]
+__all__ = [BidInfo, BidStore, utils]
--- cancelot/strategy.py
+++
@@ -1,6 +0,0 @@
-'''Utility functions bordering on bidding strategy (should be removed?).'''
-
-# FIXME: refs to web3 object
-
-from . import utils
-
