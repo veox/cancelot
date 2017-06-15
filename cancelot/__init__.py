9fc79178aa9d83a9a98493c5823a786dbc3b33f4
Noel Maersk <veox+gits@veox.pw>
2017-06-15 18:22:27 +0300

    cancelot: typo, `Exception` -> `Error`.

--- cancelot/__init__.py
+++ cancelot/__init__.py
@@ -1,6 +1,6 @@
 from .bidinfo import BidInfo
-from .bidstore import BidStore, EventType, LookupException
+from .bidstore import BidStore, EventType, LookupError
 
 from . import utils
 
-__all__ = [BidInfo, BidStore, EventType, LookupException, utils]
+__all__ = [BidInfo, BidStore, EventType, LookupError, utils]
