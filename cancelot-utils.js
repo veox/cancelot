f52295a2039bc1280835d80538d977488eba6027
Noel Maersk <veox+gits@veox.pw>
2017-06-25 13:06:31 +0300

    utils: typo, remove stray paren.

--- cancelot-utils.js
+++ cancelot-utils.js
@@ -16,7 +16,7 @@ function closestDown(num, arr) {
 }
 
 // TODO: get from http://ethgasstation.info/json/price.json
-var gasPricesInShannon = [1, 4, 18, 23, 27, 46]); // 2017-06-24
+var gasPricesInShannon = [1, 4, 18, 23, 27, 46]; // 2017-06-24
 
 function cancelotGP(bidder, seal) {
     var minGas = 28177; // see tx 0x2a8411294620fb0b5c5bbf710e7aeddbfb48c778c4a8d56e90a7cb51851016d6
